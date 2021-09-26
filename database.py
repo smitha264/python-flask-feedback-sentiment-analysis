e665da8dae6cd6ef4e10ecae56606b388f047d30
smitha264 <smithakd26@gmail.om>
2021-09-26 23:46:39 +0530

    added course adding functanility with db support

---
+++ database.py
@@ -0,0 +1,64 @@
+import sqlite3
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+def getConnection(dbfile="data.db"):
+    try:
+        conn = sqlite3.connect(dbfile)
+        return conn
+    except Exception as e:
+        return None
+
+def createTables():
+    #create course table
+    conn = getConnection()
+    if conn:
+        conn.execute("create table course(id integer primary key autoincrement, name varchar(50) not null, fees int not null)")
+        conn.commit()
+        conn.execute('''create table feedback(id integer primary key autoincrement, 
+                        content text not null,
+                        sentiment varchar(20),
+                        course_id integer not null,
+                        foreign key(course_id) references course(id))
+                    ''')
+        conn.commit()
+        conn.close()
+        print("created tables successfully")
+    else:
+        print("Could not connect to db")
+
+def getCourses():
+    conn = getConnection()
+    courses = conn.execute("select * from course").fetchall()
+    conn.close()
+    return courses
+
+    
+def addFeedback(feedback,course_id):
+    conn = getConnection()
+    sentiment = SentimentIntensityAnalyzer()
+    try:
+        predicted_sentiment = "Positive" if sentiment.polarity_scores(feedback)['compound'] > 0 else "Negative"
+        conn.execute("insert into feedback(id,content,course_id,sentiment) values(?,?,?,?)",(None, feedback, course_id, predicted_sentiment))
+        conn.commit()
+        return f"success! predicted sentiment is {predicted_sentiment}"
+    except Exception as e:
+        print(e)
+        return "failure"
+    finally:
+        conn.close()
+
+def addCourse(name, fees):
+    conn = getConnection()
+    try:
+        conn.execute("insert into course(id,name,fees) values(?,?,?)",(None, name,fees))
+        conn.commit()
+        return "success"
+    except Exception as e:
+        print(e)
+        return "failure"
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    #createTables()
+    print(addCourse('python',5000))
--- server.py
+++ server.py
@@ -1,15 +1,30 @@
-from flask import Flask ,render_template,request,redirect
-app=Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+import database
+app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/feedback",methods=['GET',"POST"])
-def feedback():
-    if request.method=="GET":
-        return render_template("feedback.html")
+@app.route("/course", methods=['GET','POST'])
+def course():
+    if request.method == "GET":
+        status = request.args.get("status",0)
+        return render_template("course.html", status=status,courses= database.getCourses())
     else:
-        user_feedback=request.form['feedback']
-        print(user_feedback)
-        return redirect("/feedback")
+        name = request.form['name']
+        fees = request.form['fees']
+        status = database.addCourse(name, fees)
+        return redirect("/course?status="+status)
+
+@app.route("/feedback", methods=['GET', 'POST'])
+def feedback():
+    if request.method == "GET":
+        status = request.args.get("status",0)
+        print(status)
+        return render_template("feedback.html",status=status, courses=database.getCourses())
+    else:
+        user_feedback = request.form['feedback']
+        course_id = request.form['course']
+        status = database.addFeedback(user_feedback, course_id)
+        return redirect("/feedback?status="+status)
 app.run(debug=True)
